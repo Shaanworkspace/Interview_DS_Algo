49ca26aea184466cc5352884720ef22ff4adbae8
MAZHAR <iiestshibpur605@gmail.com>
2026-03-26 09:18:28 +0530

    Added YouTube Link

--- Arrays/Cumulative_Sum(Prefix Array)/Equal Sum Grid Partition II.cpp
+++ Arrays/Cumulative_Sum(Prefix Array)/Equal Sum Grid Partition II.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn    : 
+    MY YOUTUBE VIDEO ON THIS Qn    : https://www.youtube.com/watch?v=Q2kDj7gy9_8
     Company Tags                   : Will update later
     Leetcode Link                  : https://leetcode.com/problems/equal-sum-grid-partition-ii/description/
 */
