285722ca9da5e08a265d5b1fafdda0a8e7b731e9
MAZHAR <iiestshibpur605@gmail.com>
2026-04-06 10:13:55 +0530

    Added YouTube Link

--- DP/Knapsack Series/Subset Sum Problem.cpp
+++ DP/Knapsack Series/Subset Sum Problem.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=Ni9RwDZdzrU
     Company Tags                : Amazon, Microsoft
     Problem Link                : https://www.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1
 */
